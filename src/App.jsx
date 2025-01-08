655519b7a9dfa696115e08d67166b8dab6b70654
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2025-01-08 19:33:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,82 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Element } from "react-scroll";
-import Header1 from './components/Header1.jsx';
-import Hero from './components/Hero.jsx';
-import T1 from "./components/T1.jsx";
-import T2 from "./components/T2.jsx";
-import T3 from "./components/T3.jsx";
-import OurServices from "./components/OurServices.jsx";
-import ChooseUs from "./components/ChooseUs.jsx";
-import ClientStats from "./components/ClientStats.jsx";
-import Testimonial from "./components/Testimonial.jsx";
-import FAQ from './components/FAQ.jsx';
-import StartNow from "./components/StartNow.jsx";
-import Footer from "./components/Footer.jsx";
-import ConsultationForm from "./components/ConsultationForm.jsx";
-import TeamSlider from "./components/TeamSlider.jsx";
-import HowWeWork from "./components/HowWeWork.jsx";
-import LatestBlog from "./components/LatestBlog.jsx";
-import Footer2 from "./components/Footer2.jsx";
 
+// Importing Components
+import Header1 from "./components/Header1";
+import Hero from "./components/Hero";
+import T1 from "./components/T1";
+import T2 from "./components/T2";
+import T3 from "./components/T3";
+import OurServices from "./components/OurServices";
+import ChooseUs from "./components/ChooseUs";
+import ClientStats from "./components/ClientStats";
+import Testimonial from "./components/Testimonial";
+import FAQ from "./components/FAQ";
+import StartNow from "./components/StartNow";
+import Footer from "./components/Footer";
+import ConsultationForm from "./components/ConsultationForm";
+import TeamSlider from "./components/TeamSlider";
+import HowWeWork from "./components/HowWeWork";
+import LatestBlog from "./components/LatestBlog";
+import Footer2 from "./components/Footer2";
+import IndustryPage from "./IndustryPage";
+// Add a NotFoundPage component
+import ScrollToTop from './ScrollToTop'
 const App = () => {
   return (
     <Router>
+      <ScrollToTop/>
+      {/* Header */}
       <Header1 />
-      <Element name="hero">
-        <Hero />
-      </Element>
-      <Element name="t1">
-        <T1 />
-      </Element>
-      <T2 />
-      <T3 />
-      <Element name="ourservices">
-        <OurServices />
-      </Element>
-      <ChooseUs />
-      <Element name="latestblog">
-        <LatestBlog />
-      </Element>
-      <HowWeWork />
-      <Element name="consultationform">
-        <ConsultationForm />
-      </Element>
-      <Element name="ts">
-        <TeamSlider/>
-      </Element>
-      <Element name="faq">
-        <FAQ />
-      </Element>
-      <StartNow />
+
+      {/* Routes for Navigation */}
+      <Routes>
+        {/* Main Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Element name="hero">
+                <Hero />
+              </Element>
+              <Element name="t1">
+                <T1 />
+              </Element>
+              <T2 />
+              <T3 />
+              <Element name="ourservices">
+                <OurServices />
+              </Element>
+              <ChooseUs />
+              <Element name="latestblog">
+                <LatestBlog />
+              </Element>
+              <HowWeWork />
+              <Element name="consultationform">
+                <ConsultationForm />
+              </Element>
+              <Element name="ts">
+                <TeamSlider />
+              </Element>
+              <Element name="faq">
+                <FAQ />
+              </Element>
+              <StartNow />
+            </>
+          }
+        />
+
+        {/* Dynamic Industry Page */}
+        <Route path="/industry/:name" element={<IndustryPage />} />
+
+        {/* 404 Page */}
+        
+      </Routes>
+
+      {/* Footer */}
       <Footer />
-      <Footer2/>
+      <Footer2 />
     </Router>
   );
 };
